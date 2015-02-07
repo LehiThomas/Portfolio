41d32e71b4cac4504d62a52bb6ab5a09488c4cb2
Lehi Thomas <Thomas-Lehi@users.noreply.github.com>
2015-02-07 13:03:50 -0700

    Create MyCollection.java

---
+++ Collectionssandbox/MyCollection.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+public class MyCollection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "My Collection\n" ); 
+		
+		List lst = new ArrayList();  
+	      lst.add("Lehi"); 
+	      lst.add("Austin");
+	      lst.add("Vaughn");
+	      lst.add("Richard");
+	      lst.add("Jonathan");
+	      System.out.println("My groups consists of: ");
+	      System.out.println(lst);     
+		
+		
+		
+		
+		
+		
+	}
+
+}
